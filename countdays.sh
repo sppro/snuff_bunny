09f126de80a971d7a3e32814b68f3f0534f794a5
sppro <bouncinback@msn.com>
2018-02-02 13:35:34 -0800

    creating a blank repository

--- countdays.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# File: countdays.sh
-
-days=(Mon Tue Wed Thu Fri Sat Sun)
-
-for day in ${days[*]}
-do
-	echo "$day = $(egrep -c $day gitLog.txt)"
-done
--- readme.txt
+++
@@ -1,4 +0,0 @@
-welcome to My First Repo
-working to learn Git.
-I added this new line in the update-readme branch
-one more line
